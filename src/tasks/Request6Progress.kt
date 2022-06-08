b20c345f5cc58c160efa897719e916ed549d34c5
Petr Filaretov <petr.filaretov@gmail.com>
2022-06-08 18:14:58 +0300

    implement progress bar with incremental updates on UI

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,18 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    var results = emptyList<User>()
+    for ((index, repo) in repos.withIndex()) {
+        val repoContributors = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        results = (results + repoContributors).aggregate()
+        updateResults(results, index == repos.lastIndex)
+    }
 }
