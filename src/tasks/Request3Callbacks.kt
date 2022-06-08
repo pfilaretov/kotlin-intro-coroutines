325e62bba839803e0ed816330ba9489ec86e5ef4
Petr Filaretov <petr.filaretov@gmail.com>
2022-06-08 15:41:39 +0300

    blocking, background, callback implementations

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -5,23 +5,34 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
 
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
+    log.info("loadContributorsCallbacks()")
+
     service.getOrgReposCall(req.org).onResponse { responseRepos ->
+        log.info("got repos response")
         logRepos(req, responseRepos)
+
         val repos = responseRepos.bodyList()
+        val latch = CountDownLatch(repos.size)
         val allUsers = mutableListOf<User>()
         for (repo in repos) {
+            log.info("get $repo contributors...")
             service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
                 allUsers += users
+                latch.countDown()
             }
         }
-        // TODO: Why this code doesn't work? How to fix that?
+
+        latch.await()
+        log.info("updating results on UI")
         updateResults(allUsers.aggregate())
     }
+
 }
 
 inline fun <T> Call<T>.onResponse(crossinline callback: (Response<T>) -> Unit) {
