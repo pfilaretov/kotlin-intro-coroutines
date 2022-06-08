3a87507bdc7d489822e68bd2acf740b84b1b755f
Petr Filaretov <petr.filaretov@gmail.com>
2022-06-08 15:43:55 +0300

    coroutines concurrent implementation

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,19 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val deferredList: List<Deferred<List<User>>> = repos.map { repo ->
+        async (Dispatchers.Default) {
+            log.info("starting loading for $repo...")
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+    deferredList.awaitAll().flatten().aggregate()
 }
